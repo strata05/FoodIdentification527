b4d98b9ff8e333e843b7985d0d905a698df1431a
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-08-29 12:26:29 +1200

    mount image router

--- python/routers/__init__.py
+++ python/routers/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from . import user
+from . import images
 
 router = APIRouter()
 router.include_router(user.router, prefix="/user", tags=["user"])
+router.include_router(images.router)
